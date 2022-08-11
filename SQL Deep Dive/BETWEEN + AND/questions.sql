cf54e5a6d7f3c3c02f529835570430c069c8822b
legendsaintwalker <110877441+legendsaintwalker@users.noreply.github.com>
2022-08-11 16:47:52 +0200

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -4,8 +4,13 @@
 /*
 * Write your query here
 */
+select age, firstname, income, lastname from "public"."customers"
+where (age between 30 and 50) and income < 50000;
+
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
 * Write your query here
 */
+select avg(income) from "public"."customers"
+where age between 20 and 50;
