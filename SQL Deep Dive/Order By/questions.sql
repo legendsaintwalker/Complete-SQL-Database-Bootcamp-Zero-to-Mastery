5bb3a17993542050b1900d6c18c71ec885e8c10b
legendsaintwalker <110877441+legendsaintwalker@users.noreply.github.com>
2022-08-13 09:20:45 +0200

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,6 +3,9 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
+select first_name, last_name from "public"."employees"
+order by first_name, last_name desc 
+;
 
 
 /*
@@ -10,6 +13,8 @@
 * Table: employees
 * Question: Sort employees by age
 */
+select age(birth_date) from "public"."employees"
+order by age(birth_date);
 
 
 /*
@@ -17,3 +22,6 @@
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+select * from "public"."employees"
+where first_name ilike 'K%'
+order by hire_date;
