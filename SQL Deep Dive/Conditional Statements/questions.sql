f4998aa772dabef5ae9254e1cf6e3277688ebc57
legendsaintwalker <110877441+legendsaintwalker@users.noreply.github.com>
2022-08-17 13:36:57 +0200

    Update questions.sql

--- SQL Deep Dive/Conditional Statements/questions.sql
+++ SQL Deep Dive/Conditional Statements/questions.sql
@@ -5,3 +5,12 @@
 * if it's between 10 and 20 you show 'average' 
 * and of is lower than or equal to 10 you show 'cheap'.
 */
+select p.price,
+    p.category,
+   case 
+        when p.price > 20 then 'expensive'
+        when p.price >= 10 or p.price <= 20 then 'average'
+        when p.price <= 10 then 'cheap'
+        end 
+     as "rating"
+from products as p
